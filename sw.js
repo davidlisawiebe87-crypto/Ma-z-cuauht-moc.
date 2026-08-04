83523a42dfc25c35f333eef46d2c0fcce67fe93a
davidlisawiebe87-crypto <davidlisawiebe87@gmail.com>
2026-08-04 13:44:49 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE = 'maiz-shell-v2';
-const SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+const CACHE = 'maiz-shell-v3';
+const SHELL = [
+  '/',
+  '/index.html',
+  '/instalar.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.webmanifest',
+  '/icon.svg',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
@@ -7,7 +17,11 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    ))
+  );
   self.clients.claim();
 });
 
@@ -15,9 +29,14 @@ self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.pathname.includes('/.netlify/functions/')) return;
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    const clone = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, clone));
-    return response;
-  }).catch(() => caches.match(event.request)));
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
